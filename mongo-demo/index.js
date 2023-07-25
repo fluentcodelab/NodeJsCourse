56fdf985059eb7c39ff09db94b0cf98724f9fb9d
Gabin Dongmo <gdongmo@gmail.com>
2023-07-25 09:32:21 -0400

    Counting

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -27,25 +27,12 @@ async function createCourse() {
 }
 
 async function getCourses() {
-    // or
-    // and
-
-    const courses = await Course
-        // .find({author: 'Mosh', isPublished: true})
-
-        // Starts with 'Mosh'
-        .find({author: /^Mosh/})
-
-        // Ends with 'Hamedani'
-        .find({author: /Hamedani$/i})   // i makes the expression case-insensitive
-
-        // Contains 'Mosh'
-        .find({author: /.*Mosh.*/})
-
+    const count = await Course
+        .find({author: 'Mosh', isPublished: true})
         .limit(10)
         .sort({name: 1})
-        .select({name: 1, tags: 1});
-    console.log(courses);
+        .count();
+    console.log(count);
 }
 
 getCourses();
