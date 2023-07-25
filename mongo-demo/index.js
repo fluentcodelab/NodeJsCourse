80a64d7ddf273929b42d52dbf5e02ba226e1caa4
Gabin Dongmo <gdongmo@gmail.com>
2023-07-25 08:45:22 -0400

    Schemas

---
+++ mongo-demo/index.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+
+mongoose.connect('mongodb://127.0.0.1:27017/playground')
+    .then(() => console.log('Connected to MongoDB...'))
+    .catch(err => console.error('Could not connect to MongoDB...', err));
+
+const courseSchema = mongoose.Schema({
+    name: String,
+    author: String,
+    tags: [String],
+    date: {type: Date, default: Date.now()},
+    isPublished: Boolean
+});
