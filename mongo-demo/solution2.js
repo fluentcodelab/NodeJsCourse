6898c5378f8739387fea0f7fcd307f9b33e379ea
Gabin Dongmo <gdongmo@gmail.com>
2023-09-17 17:53:18 -0400

    Exercises

---
+++ mongo-demo/solution2.js
@@ -0,0 +1,30 @@
+import mongoose from 'mongoose';
+
+mongoose.connect('mongodb://127.0.0.1:27017/mongo-exercises')
+    .then(() => console.log('Connected to MongoDB...'))
+    .catch(err => console.error('Could not connect to MongoDB...', err));
+
+const courseSchema = mongoose.Schema({
+    name: String,
+    author: String,
+    tags: [String],
+    date: Date,
+    isPublished: Boolean,
+    price: Number
+});
+
+const Course = mongoose.model('Course', courseSchema);
+
+async function getCourses() {
+    return await Course
+        .find({isPublished: true, tags: {$in: ['frontend', 'backend']}})
+        .sort({price: -1})
+        .select({name: 1, author: 1, price: 1});
+}
+
+async function run() {
+    const courses = await getCourses();
+    console.log(courses);
+}
+
+run();
