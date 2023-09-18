9bca1c4f821114597c7fb3c2bf4132e25ab2ba84
Gabin Dongmo <gdongmo@gmail.com>
2023-09-18 01:23:32 -0400

    Project - Add persistence to Genres API

--- vidly/index.js
+++ vidly/index.js
@@ -1,5 +1,11 @@
+import mongoose from "mongoose";
 import express from "express";
 import genres from "./routes/genres.js";
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/vidly")
+  .then(() => console.log("Connected to MongoDB..."))
+  .catch((err) => console.error("Could not connect to MongoDB...", err));
 
 const app = express();
 app.use(express.json()); // Adds middleware that turns request body in json objects
