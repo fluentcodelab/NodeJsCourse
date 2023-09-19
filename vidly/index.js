b1b1ebb174eb393fad8688414b617961b3d88e65
Gabin Dongmo <gdongmo@gmail.com>
2023-09-18 23:27:34 -0400

    Project - Build the Rentals API

--- vidly/index.js
+++ vidly/index.js
@@ -3,6 +3,7 @@ import express from "express";
 import genres from "./routes/genres.js";
 import customers from "./routes/customers.js";
 import movies from "./routes/movies.js";
+import rentals from "./routes/rentals.js";
 
 mongoose
   .connect("mongodb://127.0.0.1:27017/vidly")
@@ -14,6 +15,7 @@ app.use(express.json()); // Adds middleware that turns request body in json obje
 app.use("/api/genres", genres);
 app.use("/api/customers", customers);
 app.use("/api/movies", movies);
+app.use("/api/rentals", rentals);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
---
+++ vidly/models/rental.js
@@ -0,0 +1,68 @@
+import mongoose from "mongoose";
+import Joi from "joi";
+
+export const Rental = mongoose.model(
+  "Rental",
+  new mongoose.Schema({
+    customer: {
+      type: new mongoose.Schema({
+        name: {
+          type: String,
+          required: true,
+          minlength: 5,
+          maxlength: 50,
+        },
+        isGold: {
+          type: Boolean,
+          default: false,
+        },
+        phone: {
+          type: String,
+          required: true,
+          minlength: 5,
+          maxlength: 50,
+        },
+      }),
+      required: true,
+    },
+    movie: {
+      type: new mongoose.Schema({
+        title: {
+          type: String,
+          required: true,
+          trim: true,
+          minlength: 5,
+          maxlength: 255,
+        },
+        dailyRentalRate: {
+          type: Number,
+          required: true,
+          min: 0,
+          max: 255,
+        },
+      }),
+      required: true,
+    },
+    dateOut: {
+      type: Date,
+      required: true,
+      default: Date.now,
+    },
+    dateReturned: {
+      type: Date,
+    },
+    rentalFee: {
+      type: Number,
+      min: 0,
+    },
+  }),
+);
+
+export function validate(rental) {
+  const schema = Joi.object({
+    customerId: Joi.string().required(),
+    movieId: Joi.string().required(),
+  });
+
+  return schema.validate(rental);
+}
