9009f8da39d408309196ec15413485a51b5ff14c
Gabin Dongmo <gdongmo@gmail.com>
2023-09-19 09:47:05 -0400

    Creating the User model

---
+++ vidly/models/user.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+import Joi from "joi";
+
+export const User = mongoose.model(
+  "User",
+  new mongoose.Schema({
+    name: {
+      type: String,
+      required: true,
+      minlength: 5,
+      maxlength: 50,
+    },
+    email: {
+      type: String,
+      required: true,
+      minlength: 5,
+      maxlength: 255,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 5,
+      maxlength: 1024,
+    },
+  }),
+);
+
+export function validate(user) {
+  const schema = Joi.object({
+    name: Joi.string().min(5).max(50).required(),
+    email: Joi.string().min(5).max(255).required().email(),
+    password: Joi.string().min(5).max(255).required(),
+  });
+
+  return schema.validate(user);
+}
