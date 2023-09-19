916c666a9cf4d4241214961e787ea8d7ac48c193
Gabin Dongmo <gdongmo@gmail.com>
2023-09-18 22:09:31 -0400

    Using an array of subdocuments

--- mongo-demo/embedding.js
+++ mongo-demo/embedding.js
@@ -17,14 +17,14 @@ const Course = mongoose.model(
   "Course",
   new mongoose.Schema({
     name: String,
-    author: authorSchema,
+    authors: [authorSchema],
   }),
 );
 
-async function createCourse(name, author) {
+async function createCourse(name, authors) {
   const course = new Course({
     name,
-    author,
+    authors,
   });
 
   const result = await course.save();
@@ -56,6 +56,26 @@ async function updateAuthor(courseId) {
   console.log(course);
 }
 
-// createCourse("Node Course", new Author({ name: "Mosh" }));
+async function addAuthor(courseId, author) {
+  const course = await Course.findById(courseId);
+  course.authors.push(author);
+  course.save();
+  console.log(course);
+}
 
-updateAuthor("6508fad6fad2b79e552e8ca3");
+async function removeAuthor(courseId, authorId) {
+  const course = await Course.findById(courseId);
+  const author = course.authors.id(authorId);
+  author.deleteOne();
+  course.save();
+}
+
+// createCourse("Node Course", [
+//   new Author({ name: "John Doe" }),
+//   new Author({ name: "Jane Doe" }),
+// ]);
+
+// updateAuthor("6508fad6fad2b79e552e8ca3");
+
+// addAuthor("6508ff00b80a9bd6d0e48c12", new Author({ name: "Alpha Blondy" }));
+removeAuthor("6508ff00b80a9bd6d0e48c12", "6508ff00b80a9bd6d0e48c10");
