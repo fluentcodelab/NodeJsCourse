c153c25ba9573ae81e081a3e451392de5ccf0a6d
Gabin Dongmo <gdongmo@gmail.com>
2023-09-20 14:07:45 -0400

    Logging errors (using winston, could also be pino, or other package)

--- vidly/middleware/error.js
+++ vidly/middleware/error.js
@@ -1,4 +1,20 @@
+import { createLogger, format, transports } from "winston";
+
+const { combine, timestamp, label, prettyPrint } = format;
+const logger = createLogger({
+  format: combine(
+    timestamp({
+      format: "MMM-DD-YYYY HH:mm:ssZ",
+    }),
+    prettyPrint(),
+  ),
+  transports: [
+    new transports.Console(),
+    new transports.File({ filename: "error.log", level: "error" }),
+  ],
+});
+
 export function error(err, req, res, next) {
-  // Log the exception
+  logger.error(err.message, err);
   res.status(500).send("Something failed");
 }
