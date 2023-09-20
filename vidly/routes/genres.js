9947d2351410260a4416519e329d30051075697e
Gabin Dongmo <gdongmo@gmail.com>
2023-09-20 04:06:13 -0400

    Protecting routes

--- vidly/routes/genres.js
+++ vidly/routes/genres.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { Genre, validate } from "../models/genre.js";
+import auth from "../middleware/auth.js";
 
 const router = express.Router();
 
@@ -8,7 +9,7 @@ router.get("/", async (req, res) => {
   res.send(genres);
 });
 
-router.post("/", async (req, res) => {
+router.post("/", auth, async (req, res) => {
   const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
