f9e853dd12e1f607460aa9c08f421470370fd13b
Gabin Dongmo <gdongmo@gmail.com>
2023-09-20 21:31:43 -0400

    Extracting Db loader

--- vidly/index.js
+++ vidly/index.js
@@ -1,15 +1,16 @@
 import "winston-mongodb";
 import { createLogger, format, transports } from "winston";
 import "express-async-errors";
-import mongoose from "mongoose";
 import express from "express";
 import Joi from "joi";
 import objectId from "joi-objectid";
 import config from "config";
-import { loadRoutes } from "./startup/loadRoutes.js";
+import { routes } from "./startup/routes.js";
+import { db } from "./startup/db.js";
 
 const app = express();
-loadRoutes(app);
+routes(app);
+db();
 
 const { combine, timestamp, label, prettyPrint } = format;
 const logger = createLogger({
@@ -42,8 +43,8 @@ process.on("unhandledRejection", (e) => {
   throw e;
 });
 
-const p = Promise.reject(new Error("Something failed terribly..."));
-p.then(() => console.log("Done"));
+// const p = Promise.reject(new Error("Something failed terribly..."));
+// p.then(() => console.log("Done"));
 
 if (!config.get("jwtPrivateKey")) {
   console.error("FATAL ERROR: jwtPrivateKey is not defined.");
@@ -51,11 +52,6 @@ if (!config.get("jwtPrivateKey")) {
 }
 
 Joi.objectId = objectId(Joi);
-
-mongoose
-  .connect("mongodb://127.0.0.1:27017/vidly")
-  .then(() => console.log("Connected to MongoDB..."))
-  .catch((err) => console.error("Could not connect to MongoDB...", err));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
