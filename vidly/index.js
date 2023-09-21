f3d8346a4eee34ff1ef9841be41a9a1aaa236440
Gabin Dongmo <gdongmo@gmail.com>
2023-09-20 21:21:55 -0400

    Extracting routes

--- vidly/index.js
+++ vidly/index.js
@@ -6,13 +6,10 @@ import express from "express";
 import Joi from "joi";
 import objectId from "joi-objectid";
 import config from "config";
-import { error } from "./middleware/error.js";
-import genres from "./routes/genres.js";
-import customers from "./routes/customers.js";
-import movies from "./routes/movies.js";
-import rentals from "./routes/rentals.js";
-import users from "./routes/users.js";
-import auth from "./routes/auth.js";
+import { loadRoutes } from "./startup/loadRoutes.js";
+
+const app = express();
+loadRoutes(app);
 
 const { combine, timestamp, label, prettyPrint } = format;
 const logger = createLogger({
@@ -59,16 +56,6 @@ mongoose
   .connect("mongodb://127.0.0.1:27017/vidly")
   .then(() => console.log("Connected to MongoDB..."))
   .catch((err) => console.error("Could not connect to MongoDB...", err));
-
-const app = express();
-app.use(express.json()); // Adds middleware that turns request body in json objects
-app.use("/api/genres", genres);
-app.use("/api/customers", customers);
-app.use("/api/movies", movies);
-app.use("/api/rentals", rentals);
-app.use("/api/users", users);
-app.use("/api/auth", auth);
-app.use(error);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
