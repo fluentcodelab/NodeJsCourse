5043defe1c9861d9717af02786c463dbd24bc535
Gabin Dongmo <gdongmo@gmail.com>
2023-09-20 23:43:14 -0400

    Extracting config logic

--- vidly/index.js
+++ vidly/index.js
@@ -1,20 +1,16 @@
-import "winston-mongodb";
 import "express-async-errors";
 import express from "express";
 import Joi from "joi";
 import objectId from "joi-objectid";
-import config from "config";
+import "./startup/logging.js";
+import { setConfig } from "./startup/config.js";
 import { routes } from "./startup/routes.js";
 import { db } from "./startup/db.js";
 
 const app = express();
 routes(app);
 db();
-
-if (!config.get("jwtPrivateKey")) {
-  console.error("FATAL ERROR: jwtPrivateKey is not defined.");
-  process.exit(1);
-}
+setConfig();
 
 Joi.objectId = objectId(Joi);
 
