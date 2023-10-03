b9dc7a6af37590c99126a3726ffaeeffdee50c75
Gabin Dongmo <gabin.dongmo@gmail.com>
2023-10-03 01:13:02 -0400

    Grouping tests

--- testing-demo/tests/lib.test.js
+++ testing-demo/tests/lib.test.js
@@ -1,16 +1,19 @@
 const lib = require(`../lib`);
 
-test(`absolute - should return a positive number is input is positive`, () => {
-    const result = lib.absolute(1);
-    expect(result).toBe(1);
-});
+describe(`absolute`, () => {
+    it(`should return a positive number is input is positive`, () => {
+        const result = lib.absolute(1);
+        expect(result).toBe(1);
+    });
 
-test(`absolute - should return a positive number is input is negative`, () => {
-    const result = lib.absolute(-1);
-    expect(result).toBe(1);
-});
+    it(`should return a positive number is input is negative`, () => {
+        const result = lib.absolute(-1);
+        expect(result).toBe(1);
+    });
 
-test(`absolute - should return 0 is input is 0`, () => {
-    const result = lib.absolute(0);
-    expect(result).toBe(0);
-});
+    it(`should return 0 is input is 0`, () => {
+        const result = lib.absolute(0);
+        expect(result).toBe(0);
+    });
+})
+
