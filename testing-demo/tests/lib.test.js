b90296888ad403bb26ed4929ecfed6ffbe3b68d3
Gabin Dongmo <gabin.dongmo@gmail.com>
2023-10-03 01:10:13 -0400

    Testing numbers

--- testing-demo/tests/lib.test.js
+++ testing-demo/tests/lib.test.js
@@ -1,3 +1,16 @@
-test(`Our first test`, () => {
+const lib = require(`../lib`);
 
-})
+test(`absolute - should return a positive number is input is positive`, () => {
+    const result = lib.absolute(1);
+    expect(result).toBe(1);
+});
+
+test(`absolute - should return a positive number is input is negative`, () => {
+    const result = lib.absolute(-1);
+    expect(result).toBe(1);
+});
+
+test(`absolute - should return 0 is input is 0`, () => {
+    const result = lib.absolute(0);
+    expect(result).toBe(0);
+});
