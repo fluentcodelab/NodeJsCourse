eed53393b569367ac393bf45a49e4d15b950f001
Gabin Dongmo <gabin.dongmo@gmail.com>
2023-10-03 02:41:49 -0400

    Creating simple mock functions

--- testing-demo/tests/lib.test.js
+++ testing-demo/tests/lib.test.js
@@ -1,4 +1,5 @@
 const lib = require(`../lib`);
+const db = require(`../db`);
 
 describe(`absolute`, () => {
     it(`should return a positive number is input is positive`, () => {
@@ -69,4 +70,16 @@ describe(`registerUser`, () => {
         expect(result).toMatchObject({username: `Doe`});
         expect(result.id).toBeGreaterThan(0);
     });
+});
+
+describe(`applyDiscount`, () => {
+    it(`should apply 10% discount if customer has more than 10 points`, () => {
+        db.getCustomerSync = function (customerId) {
+            console.log(`Fake reading customer...`);
+            return {id: customerId, points: 20};
+        }
+        const order = {customerId: 1, totalPrice: 10};
+        lib.applyDiscount(order);
+        expect(order.totalPrice).toBe(9);
+    })
 });
