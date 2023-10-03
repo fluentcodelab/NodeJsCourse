6609de857646e2ee2c76d80711bd0602e7500e65
Gabin Dongmo <gabin.dongmo@gmail.com>
2023-10-03 01:29:29 -0400

    Testing strings

--- testing-demo/tests/lib.test.js
+++ testing-demo/tests/lib.test.js
@@ -15,5 +15,11 @@ describe(`absolute`, () => {
         const result = lib.absolute(0);
         expect(result).toBe(0);
     });
-})
+});
 
+describe(`greet`, () => {
+   it(`should return the greeting message`, () => {
+       const result = lib.greet(`John Doe`);
+       expect(result).toMatch(/Welcome John Doe/);
+   });
+});
