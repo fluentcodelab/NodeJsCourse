c0090de62ab2470fd6fe4f3d2836ecbc52fe552d
Gabin Dongmo <gabin.dongmo@gmail.com>
2023-10-18 00:48:33 -0400

    Populating the test db

--- vidly/tests/integration/genres.test.js
+++ vidly/tests/integration/genres.test.js
@@ -1,5 +1,6 @@
 import request from "supertest";
 import { server } from "../../index";
+import { Genre } from "../../models/genre.js";
 
 let serverInstance;
 
@@ -9,12 +10,22 @@ describe(`/api/genres`, () => {
   });
   afterEach(async () => {
     await serverInstance.close();
+    await Genre.deleteMany({});
   });
 
   describe(`GET /`, () => {
     it(`should return all genres`, async () => {
+      await Genre.collection.insertMany([
+        { name: "genre1" },
+        { name: "genre2" },
+      ]);
+
       const res = await request(serverInstance).get(`/api/genres`);
+
       expect(res.status).toBe(200);
+      expect(res.body.length).toBe(2);
+      expect(res.body.some((g) => g.name === `genre1`)).toBeTruthy();
+      expect(res.body.some((g) => g.name === `genre2`)).toBeTruthy();
     });
   });
 });
