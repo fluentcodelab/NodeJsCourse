d4ffa3119580b6c99f63943fd80656c50d7dbb2e
Gabin Dongmo <gabin.dongmo@gmail.com>
2023-10-23 22:01:35 -0400

    Looking up an object

--- vidly/routes/returns.js
+++ vidly/routes/returns.js
@@ -1,4 +1,5 @@
 import express from "express";
+import { Rental } from "../models/rental.js";
 
 const router = express.Router();
 
@@ -6,6 +7,12 @@ router.post("/", async (req, res, next) => {
   if (!req.body.customerId)
     return res.status(400).send(`customerId not provided`);
   if (!req.body.movieId) return res.status(400).send(`movieId not provided`);
+
+  const rental = await Rental.findOne({
+    "customer._id": req.body.customerId,
+    "movie._id": req.body.movieId,
+  });
+  if (!rental) return res.status(404).send(`Rental not found.`);
 
   res.status(401).send(`Unauthorized`);
 });
