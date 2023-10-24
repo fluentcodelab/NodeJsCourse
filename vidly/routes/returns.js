52bcf6cbd22d6bce42a706fbe5f18a3306a08cd1
Gabin Dongmo <gabin.dongmo@gmail.com>
2023-10-23 22:15:40 -0400

    Testing if rental is processed

--- vidly/routes/returns.js
+++ vidly/routes/returns.js
@@ -5,14 +5,17 @@ const router = express.Router();
 
 router.post("/", async (req, res, next) => {
   if (!req.body.customerId)
-    return res.status(400).send(`customerId not provided`);
-  if (!req.body.movieId) return res.status(400).send(`movieId not provided`);
+    return res.status(400).send(`customerId not provided.`);
+  if (!req.body.movieId) return res.status(400).send(`movieId not provided.`);
 
   const rental = await Rental.findOne({
     "customer._id": req.body.customerId,
     "movie._id": req.body.movieId,
   });
   if (!rental) return res.status(404).send(`Rental not found.`);
+
+  if (rental.dateReturned)
+    return res.status(400).send(`Return already processed.`);
 
   res.status(401).send(`Unauthorized`);
 });
